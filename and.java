00b945e76599e1d685c5414347157bb0a38940f2
DeepikaSugumar <s.deepisugu@gmail.com>
2016-08-03 21:54:51 -0700

    fdgdg
    retewe

---
+++ and.java
@@ -0,0 +1,25 @@
+package gfgfg;
+
+import java.util.Scanner;
+
+public class and {
+
+	public static void main(String[] args) {
+		int a,b;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		if(a>b)
+		{
+		System.out.println(a);
+		}
+		else
+		{
+			System.out.println(b);
+		}
+
+	}
+
+}

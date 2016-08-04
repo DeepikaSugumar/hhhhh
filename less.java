a3461c8270ffd35ff5a23bba23a2eb029155d699
DeepikaSugumar <s.deepisugu@gmail.com>
2016-08-03 21:57:40 -0700

    gdfgdf
    ertreter

---
+++ less.java
@@ -0,0 +1,21 @@
+package gfgfg;
+
+import java.util.Scanner;
+
+public class less {
+
+	public static void main(String[] args) {
+		int a,b;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		
+		System.out.println(a==b);
+		
+
+
+	}
+
+}

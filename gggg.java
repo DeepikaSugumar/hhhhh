c4ba9899c1dd155f5ba6996b85ec709176f0ab0f
DeepikaSugumar <s.deepisugu@gmail.com>
2016-08-04 01:40:11 -0700

    iuoiu
    hkjk

---
+++ gggg.java
@@ -0,0 +1,19 @@
+package gfgfg;
+
+import java.util.Scanner;
+
+public class gggg {
+
+	public static void main(String[] args) {
+		int a,b;
+		System.out.println("Enter the first number");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		System.out.println("Enter the second number");
+		b=s.nextInt();
+		
+		System.out.println(a!=b);
+
+	}
+
+}
